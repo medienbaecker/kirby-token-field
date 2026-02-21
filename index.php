069474516060b55dce62478d14445f5686e65e0c
Thomas Günther <mail@medienbaecker.com>
2026-02-21 15:39:19 +0100

    Rename query to template, add dynamic options

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 
 use Kirby\Cms\App as Kirby;
+use Kirby\Field\FieldOptions;
 use Kirby\Toolkit\Str;
 
 Kirby::plugin('medienbaecker/token-field', [
@@ -11,45 +12,25 @@ Kirby::plugin('medienbaecker/token-field', [
 	'fields' => [
 		'token' => [
 			'props' => [
-				'options' => function (array $options = []): array {
+				'options' => function ($options = []) {
 					return $options;
 				},
 				'preview' => function (string $preview = 'color'): string {
 					return $preview;
 				},
-				'query' => function (string|null $query = null): string|null {
-					return $query;
+				'template' => function (string|null $template = null): string|null {
+					return $template;
 				},
 			],
 			'computed' => [
 				'options' => function (): array {
-					$result = [];
-
-					foreach ($this->options as $key => $option) {
-						if (is_string($option)) {
-							$display = null;
-							if ($this->query !== null) {
-								$display = Str::template($this->query, ['value' => (string)$key]);
-							}
-							$result[] = [
-								'value'   => (string)$key,
-								'text'    => $option,
-								'display' => $display,
-							];
-						} elseif (is_array($option)) {
-							$display = $option['display'] ?? null;
-							if ($display === null && $this->query !== null) {
-								$display = Str::template($this->query, ['value' => (string)$key]);
-							}
-							$result[] = [
-								'value'   => (string)$key,
-								'text'    => $option['label'] ?? $option['text'] ?? (string)$key,
-								'display' => $display,
-							];
-						}
+					if ($this->isDynamic()) {
+						$options = $this->resolveDynamic();
+					} else {
+						$options = $this->resolveStatic();
 					}
 
-					return $result;
+					return $this->applyTemplate($options);
 				},
 				'previewText' => function (): string {
 					$map = [
@@ -64,6 +45,57 @@ Kirby::plugin('medienbaecker/token-field', [
 				},
 			],
 			'methods' => [
+				'isDynamic' => function (): bool {
+					return is_string($this->options)
+						|| isset($this->options['type']);
+				},
+				'resolveDynamic' => function (): array {
+					$props = FieldOptions::polyfill([
+						'options' => $this->options,
+					]);
+					$options = FieldOptions::factory($props['options']);
+					$resolved = $options->render($this->model());
+
+					return array_map(fn($opt) => [
+						'value'   => (string)$opt['value'],
+						'text'    => $opt['text'],
+						'display' => null,
+					], $resolved);
+				},
+				'resolveStatic' => function (): array {
+					$result = [];
+
+					foreach ($this->options as $key => $option) {
+						if (is_string($option)) {
+							$result[] = [
+								'value'   => (string)$key,
+								'text'    => $option,
+								'display' => null,
+							];
+						} elseif (is_array($option)) {
+							$result[] = [
+								'value'   => (string)$key,
+								'text'    => $option['label'] ?? $option['text'] ?? (string)$key,
+								'display' => $option['display'] ?? null,
+							];
+						}
+					}
+
+					return $result;
+				},
+				'applyTemplate' => function (array $options): array {
+					if ($this->template === null) {
+						return $options;
+					}
+
+					foreach ($options as &$option) {
+						if ($option['display'] === null) {
+							$option['display'] = Str::template($this->template, ['value' => $option['value']]);
+						}
+					}
+
+					return $options;
+				},
 				'emptyValue' => function () {
 					return '';
 				}
